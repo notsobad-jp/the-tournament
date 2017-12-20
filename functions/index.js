cbbb01d38b1ff8b8b4b68e0043f4264324dea55e
tomomichi <tomomichi.onishi@gmail.com>
2017-12-20 14:47:49 +0900

    use firestore trigger

--- functions/index.js
+++ functions/index.js
@@ -13,57 +13,52 @@ var db = admin.firestore();
 var bucket = admin.storage().bucket('app.the-tournament.jp');
 
 
-exports.renderHTML = functions.https.onRequest((request, response) => {
-  var id = "deI7TIO7foDd5fVCbUGf";
-  var docRef = db.collection("tournaments").doc(id);
-  docRef.get().then(function(doc){
-    if(doc.exists) {
-      var tournament = doc.data();
-      var html = riot.render(bracket, {tournament: tournament, editable: false, id: id});
+exports.renderHTML = functions.firestore.document('tournaments/{id}').onWrite(event => {
+  var tournament = event.data.data();
+  var id = event.params.id;
 
-      var header = function(){/*
-        <html>
-        <head>
-          <meta charset="utf-8" />
-          <meta content='noindex' name='robots'>
-          <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  var html = riot.render(bracket, {tournament: tournament, editable: false, id: id});
+  var header = function(){/*
+    <html>
+    <head>
+      <meta charset="utf-8" />
+      <meta content='noindex' name='robots'>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-          <style>
-            * { font-family: 'Lucida Grande','Hiragino Kaku Gothic ProN', Meiryo, sans-serif; }
-            body { margin: 0; }
-            a { text-decoration: none; }
-            h1 { font-weight: lighter; font-size: 14px; }
-            h1 small, h1 small a { font-size: 65%; font-weight: normal; line-height: 1; color: #999999; }
-            body#embed{background:transparent}
-            #emb-container{margin:10px}
-            #emb-header{background:#333;color:#fff}
-            #emb-header h1{font-size:14px;margin:0;padding:5px}
-            #emb-header h1 small{margin-left:15px;}
-            #emb-header a{color:#fff}
-            #emb-body{background:#fff;padding:10px;border:1px dotted #333;}
-            #emb-body .bracket{margin-bottom:20px; overflow: auto;}
-            #emb-body #emb-footer{margin-left:10px}
-            #emb-ad{margin-bottom:5px}
-          </style>
+      <style>
+        * { font-family: 'Lucida Grande','Hiragino Kaku Gothic ProN', Meiryo, sans-serif; }
+        body { margin: 0; }
+        a { text-decoration: none; }
+        h1 { font-weight: lighter; font-size: 14px; }
+        h1 small, h1 small a { font-size: 65%; font-weight: normal; line-height: 1; color: #999999; }
+        body#embed{background:transparent}
+        #emb-container{margin:10px}
+        #emb-header{background:#333;color:#fff}
+        #emb-header h1{font-size:14px;margin:0;padding:5px}
+        #emb-header h1 small{margin-left:15px;}
+        #emb-header a{color:#fff}
+        #emb-body{background:#fff;padding:10px;border:1px dotted #333;}
+        #emb-body .bracket{margin-bottom:20px; overflow: auto;}
+        #emb-body #emb-footer{margin-left:10px}
+        #emb-ad{margin-bottom:5px}
+      </style>
 
-          <title>THE TOURNAMENT</title>
+      <title>THE TOURNAMENT</title>
 
-          <!--[if lt IE 9]>
-          <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.1/html5shiv.js"></script>
-          <![endif]-->
-          <link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/lafeber/world-flags-sprite/9a8b5ea6/stylesheets/flags16.css">
-          <link rel="stylesheet" type="text/css" href="https://app.the-tournament.jp/assets/css/tournament.css">
-        </head>
-      */}.toString().match(/(?:\/\*(?:[\s\S]*?)\*\/)/).pop().replace(/^\/\*/, "").replace(/\*\/$/, "");
+      <!--[if lt IE 9]>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.1/html5shiv.js"></script>
+      <![endif]-->
+      <link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/lafeber/world-flags-sprite/9a8b5ea6/stylesheets/flags16.css">
+      <link rel="stylesheet" type="text/css" href="https://app.the-tournament.jp/assets/css/tournament.css">
+    </head>
+  */}.toString().match(/(?:\/\*(?:[\s\S]*?)\*\/)/).pop().replace(/^\/\*/, "").replace(/\*\/$/, "");
 
-      var file = bucket.file('alpha/' + id + '.html');
-      file.save(header + html + '</html>', {
-        metadata: { contentType: 'text/html' },
-        gzip: true
-      });
-      process.on('unhandledRejection', console.dir);
-      response.send("Hello from Firebase!");
-    }
+  var file = bucket.file('alpha/' + id + '.html');
+  file.save(header + html + '</html>', {
+    metadata: { contentType: 'text/html' },
+    gzip: true
   });
+  process.on('unhandledRejection', console.dir);
+  response.send("Hello from Firebase!");
 });
